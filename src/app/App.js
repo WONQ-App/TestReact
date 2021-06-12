b8bcf672755cd5b3b36c998976f1af1c0ff722ab
rnrnstar2 <39724729+rnrnstar2@users.noreply.github.com>
2021-06-12 18:09:11 +0900

    add

--- src/app/App.js
+++ src/app/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import '@fake-db';
 import FuseAuthorization from '@fuse/core/FuseAuthorization';
 import FuseLayout from '@fuse/core/FuseLayout';
@@ -12,10 +14,26 @@ import Provider from 'react-redux/es/components/Provider';
 import { Router } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 import DateFnsUtils from '@date-io/date-fns';
+import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import PubSub from '@aws-amplify/pubsub';
+import Amplify, { I18n } from '@aws-amplify/core';
+
+import awsmobile from '../aws-exports';
 import AppContext from './AppContext';
 import { Auth } from './auth';
 import routes from './fuse-configs/routesConfig';
 import store from './store';
+// Amplify
+// 翻訳
+import { vocabularies } from '../dictionary';
+
+// Amplifyの設定
+Amplify.configure(awsmobile);
+PubSub.configure(awsmobile);
+
+I18n.putVocabularies(vocabularies);
+I18n.setLanguage('ja');
 
 const jss = create({
 	...jssPreset(),
@@ -26,6 +44,24 @@ const jss = create({
 const generateClassName = createGenerateClassName({ disableGlobal: true });
 
 const App = () => {
+	const [authState, setAuthState] = useState();
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		return onAuthUIStateChange((nextAuthState, authData) => {
+			// console.log(nextAuthState, authData)
+			setAuthState(nextAuthState);
+			setUser(authData);
+			// ユーザーグループによってサイドバーを変更
+			if (authData) {
+				if (authData.signInUserSession) {
+					const userGroup = authData.signInUserSession.accessToken.payload['cognito:groups'];
+					console.log(userGroup);
+				}
+			}
+		});
+	}, []);
+
 	return (
 		<AppContext.Provider
 			value={{
@@ -49,8 +85,38 @@ const App = () => {
 												containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99'
 											}}
 										>
-
-											<FuseLayout />
+											{/* 認証チェック */}
+											{authState === AuthState.SignedIn && user ? (
+												<div>
+													<AmplifySignOut />
+													<FuseLayout />
+												</div>
+											) : (
+												<AmplifyAuthenticator
+													slot="amplify-authenticator"
+													usernameAlias="email"
+												>
+													<AmplifySignUp
+														slot="sign-up"
+														usernameAlias="email"
+														formFields={[
+															{
+																type: 'email',
+																label: 'Eメールアドレス *',
+																placeholder: 'Eメール',
+																required: true
+															},
+															{
+																type: 'password',
+																label: 'パスワード *',
+																placeholder: 'パスワード',
+																required: true
+															}
+														]}
+													/>
+													<AmplifySignIn slot="sign-in" usernameAlias="email" />
+												</AmplifyAuthenticator>
+											)}
 										</SnackbarProvider>
 									</FuseTheme>
 								</FuseAuthorization>
